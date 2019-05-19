670e2d1872d47c30ab27b48d96df2ed49224e9c0
Siarhei <liroshenll@gmail.com>
2019-05-19 22:33:51 +0300

    Change picture extention

--- views/js/main.js
+++ views/js/main.js
@@ -532,7 +532,7 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.className = 'mover';
     elem.style.height = "100px";
     elem.style.width = "73.333px";
-    elem.src = "images/pizza.png";
+    elem.src = "images/pizza.webp";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     movingContainer.appendChild(elem);
